ca7dd1fc6efa627864b872bc6eb1660b1be3c3fa
Barthes Lucie <lucieanaisbarthes@gmail.com>
2016-03-24 11:50:10 +0100

    challenge7 fini

--- js/script.js
+++ js/script.js
@@ -2,5 +2,20 @@ jQuery(function($) {
                 $(document).ready( function() {
 
                   $('.navi').stickUp();
+                  $("#owl-demo").owlCarousel({
+
+     autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+     items : 3,
+     itemsDesktop : [1199,3],
+     itemsDesktopSmall : [979,3]
+
+ });
+ $('.js-scrollTo').on('click', function() { // Au clic sur un élément
+			var page = $(this).attr('href'); // Page cible
+			var speed = 750; // Durée de l'animation (en ms)
+			$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+			return false;
+		});
                 });
               });
